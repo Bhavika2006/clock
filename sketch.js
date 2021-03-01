78c9b060f427a92da9c26b95f34bb9de9e54b1e6
Bhavika2006 <70019267+Bhavika2006@users.noreply.github.com>
2021-03-02 01:22:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,3 @@
-var h = hour();
-var s = second();
-var m = minute();
 
 
 
@@ -11,31 +8,45 @@ function setup() {
 
 function draw() {
   background(255,255,255);  
+  var h = hour();
+  var s = second();
+  var m = minute();
+  
   angleMode(DEGREES)
-  scAngle = map(o,60,angle,0,360)
+  scAngle = map(s,0,60,0,360)
+  mnAngle = map(m,0,60,0,360)
+  hrAngle = map(h%12,0,12,0,360)
+  
   //drawing second hand 
   push();
-  translate(this.body.position.x,this.body.position.y)
+  translate(400,200)
   rotate(scAngle)
   stroke('red')
   strokeWeight(7)
   line(0,0,100,0)
+  
+arc(0, 0, 90, 300, 0, scAngle);
+  
   pop();
   // drawing hour hand 
   push();
-  translate(this.body.position.x,this.body.position.y)
-  rotate(scAngle)
+  translate(400,200)
+  rotate(mnAngle)
   stroke('blue')
   strokeWeight(7)
   line(0,0,50,0)
+  
+  arc(0, 0, 280, 280, 0, mnAngle);
   pop();
   //drawing minute hand 
   push();
-  translate(this.body.position.x,this.body.position.y)
-  rotate(scAngle)
+  translate(400,200)
+  rotate(hrAngle)
   stroke('green')
   strokeWeight(7)
   line(0,0,80,0)
+  
+  arc(0, 0, 200, 200, 0, hrAngle);
   pop();
 
   drawSprites();
